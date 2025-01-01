ddd91ca03d3c1595131c575ffd2ccd4b0d175c29
adrien-deroffignac <adrien.de_roffignac@edu.devinci.fr>
2025-01-01 16:53:46 +0100

    [ADD] Camera Settings

---
+++ Source/SmashUE/Public/CameraSettings.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CameraSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMASHUE_API UCameraSettings : public UObject
+{
+	GENERATED_BODY()
+};
